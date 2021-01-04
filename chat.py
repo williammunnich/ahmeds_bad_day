15c0da2b828383442c58809786c043a73e6b47aa
williammunnich <williammunnich@gmail.com>
2021-01-03 21:32:39 -0600

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,22 @@
+from ascii_art import middle_finger
+#im gonna be making a chat box that call people bad names
+# take user input and store to a variable 
+#return statement according if its in "if else" tree 
+# loop 
+user_input= "ligma"
+while True:
+	user_input= input("ahmed's body positivity bot ")
+	if user_input == "hello":
+	 	print("your parents weep every night that the condom broke")
+	elif user_input == "why so mean":
+		print("because your existence is an accident")	
+	elif user_input == "im having a bad day":
+		print(":)")
+	elif user_input == "my feelings are hurt":
+		print(middle_finger()) 	
+	elif user_input == "hi":
+		print("you have no reason to be double caked up on a sunday night like that")
+	else:
+		print("you stuttered th0t, try again.")
+		
+		
